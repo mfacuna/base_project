ca6f099272962ca546d94438236914f3892aa3a3
Matias Acuña <mfacuna@ing.ucsc.cl>
2020-02-09 12:35:28 -0300

    scaffolding base

---
+++ base_project/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace base_project.Models
+{
+    public partial class User
+    {
+        public Guid? UserId { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public long? Active { get; set; }
+    }
+}
---
+++ base_project/Models/db_base_projectContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace base_project.Models
+{
+    public partial class db_base_projectContext : DbContext
+    {
+        public db_base_projectContext()
+        {
+        }
+
+        public db_base_projectContext(DbContextOptions<db_base_projectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_base_project;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Mail)
+                    .HasMaxLength(255)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(255)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
